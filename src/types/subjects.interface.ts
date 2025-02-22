eac9dafe43ff670e7ad6d019aa584e9ab31db689
darkvadr <glowburn.acc@gmail.com>
2025-02-22 09:43:03 +0000

    add versioning

--- src/types/subjects.interface.ts
+++ src/types/subjects.interface.ts
@@ -1,7 +1,16 @@
 import { Subjects } from "../types/subjects.enum";
 
-export interface TicketCreatedEvent {
+// Interface representing a generic event in a channel
+interface ChannelEvent <T = object> {
+  // The topic of the event, which is an enum value from Subjects
   topic: Subjects;
+  // The version to enforce application level ordering before processing
+  version: number;
+  // The payload of the event, which contains event-specific data
+  payload: T;
+}
+
+export interface TicketCreatedEvent extends ChannelEvent {
   payload: {
     id: string;
     title: string;
